b6e2e5d9a71f000757835c097948fed4f6bcd6b5
Anderson Franciscato <andersonwsf@gmail.com>
2022-11-01 16:52:14 -0300

    Revert "Loja Php "

--- php/sotre_php/core/views/main.php
+++
@@ -1,7 +0,0 @@
-<main class="container-fluid container-pagina-inicial">
-    <div class="row">
-        <div class="col-12">
-            <h1>Página Inicial</h1>
-        </div>
-    </div>
-</main>
--- php/sotre_php/scripts/querys.sql
+++
@@ -1,5 +0,0 @@
-SELECT u.name, u.email, pp.logradouro, a.complemento, a.numero, a.bairro, a.cep, a.uf, au.tipo_endereco
-FROM addres_user au
-JOIN users u ON (u.id = au.users_id)
-JOIN address a ON (a.id = au.address_id)
-JOIN public_place pp ON (pp.id = a.public_place_id)
